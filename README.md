dbfc38e01424ecae6c246b67ecfc881e06f876e1
Kamil <56117737+KamilDominko@users.noreply.github.com>
2026-01-05 21:38:24 +0100

    panel administracyjny
    dodałem superużytkownika oraz dostosowałem panel administracyjny
    zmieniłem język swojej strony na polski

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Django5Blog
 Pierwszy projekt Django5
+
+stworzenie superużytkownika: `python manage.py createsuperuser`
+
+uruchomienie serwera lokalnie: `python manage.py runserver`
+
--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+
+# admin.site.register(Post)
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+
+    # list_display dodaje w oknie panelu administratora dodatkowe informacje na temat każdego posta
+    list_display = ["title", "slug", "author", "publish", "status"]
+
+    # list_filetr dodaje filtrowanie postów pod kątm podanych pól
+    list_filter = ["status", "created", "publish", "author"]
+
+    # lista pól do przeszukiwania postów - poasek wyszukiwania szuka frazy w podanych polach postów
+    search_fields = ["title", "body"]
+
+    # automatycznie tworzy slug z podanego tytułu
+    prepopulated_fields = {"slug": ("title",)}
+
+    # okienkowy widget do łatwiejszego dodawania autorów
+    raw_id_fields = ["author"]
+
+    # dzieli posty na hierarchie dat
+    date_hierarchy = "publish"
+
+    # domyślnie sortuje posty po tych polach
+    ordering = ["status", "publish"]
+
+    # pokazuje przy na pasku filtrów, ile jest postów zawierajacych każdy aspekt
+    show_facets = admin.ShowFacets.ALWAYS
