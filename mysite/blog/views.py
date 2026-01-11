82d25408671d36502b029c037bdfc56981f60421
Kamil <56117737+KamilDominko@users.noreply.github.com>
2026-01-11 18:11:56 +0100

    Dodanie stronicowania
    Dodałem stronicowanie, teraz lista wyświetla po 3 posty na stronie.

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.http import Http404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 def post_list(request):
-    posts = Post.published.all()
+    post_list = Post.published.all()
+    # Stronicowanie z 3 postami na stronę
+    paginator = Paginator(post_list, 3)
+    # Pobiera numer strony "page" z request, jeżeli nie ma to zwraca 1
+    page_number = request.GET.get("page", 1)
+    try:
+        posts = paginator.page(page_number)
+    except EmptyPage:
+        # Jeśli zmienna page_number jest poza zakresem, wyślij ostatnią stronę wyników
+        posts = paginator.page(paginator.num_pages)
+    except PageNotAnInteger:
+        # Jeśli page_number nie jest liczbą całkowitą zwróć pierwszą stronę
+        posts = paginator.page(1)
     return render(request, "blog/post/list.html", {"posts": posts})
 
 
