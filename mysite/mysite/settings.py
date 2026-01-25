5f61cd3bb0e6a5a72cc2a9e3b0ef39331038f302
Kamil <56117737+KamilDominko@users.noreply.github.com>
2026-01-25 15:45:27 +0100

    Własne filtry szablonu i mapa witryny
    Utworzyłem własny filtr szablonu, który zamienia tekst markdown na html.
    Dodałem mapę witryny.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -36,6 +36,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+SITE_ID = 1
 
 # Application definition
 
@@ -48,6 +49,8 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "blog.apps.BlogConfig",
     "taggit",
+    "django.contrib.sites",
+    "django.contrib.sitemaps",
 ]
 
 MIDDLEWARE = [
